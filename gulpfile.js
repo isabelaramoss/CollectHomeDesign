d27b56725a5ff70e23b32611ed276ac7c935986c
Isabela Ramos <isamrteixeira@gmail.com>
2024-01-06 23:56:29 -0300

    adicionando imagens e configurando imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens(){
+  return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
 
 function compilaSass(){
   return gulp.src('./src/styles/main.scss')
@@ -15,11 +23,11 @@ function compilaSass(){
 
 exports.default = function(){
   gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+  gulp.watch('./src/images', {ignoreInitial: false}, gulp.series(comprimeImagens));
 }
 
 /* 
-gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
-  gulp.watch('./src/images', {ignoreInitial: false}, gulp.series(comprimeImagens));
+  gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
   gulp.watch('./src/*.html', {ignoreInitial: false}, gulp.series(comprimeHtml));
 
 */
